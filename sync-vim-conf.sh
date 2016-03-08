73d25d3f30823797c017018058865e316f72bfe4
Tom Hu <h1994st@gmail.com>
2016-03-08 14:52:23 +0800

    Update sync-vim-conf.sh

--- sync-vim-conf.sh
+++ sync-vim-conf.sh
@@ -2,16 +2,17 @@
 
 set -e;
 
-mv ~/.vim ~/vimbak
-mv ~/.vimrc ~/.vimrc.bak
-git clone https://github.com/h1994st/vim-conf.git ~/.vim
-
 # Install dependencies
 SYSTEM=`uname -s`
 if [SYSTEM = "Linux"] ; then
     echo "Linux"
     sudo apt-get install git vim ctags
 fi
+
+# Back-up
+mv ~/.vim ~/vimbak
+mv ~/.vimrc ~/.vimrc.bak
+git clone https://github.com/h1994st/vim-conf.git ~/.vim
 
 # Clone Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
